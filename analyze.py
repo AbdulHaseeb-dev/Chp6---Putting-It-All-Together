f9f56bd64dd64b7d94b9413e1a1eeabfff126c65
AbdulHaseeb-dev <haseebrana.faisal@gmail.com>
2024-01-22 16:04:13 +0500

    Initial commit

---
+++ analyze.py
@@ -0,0 +1,10 @@
+import ch1text
+def compute_readibility(text):
+    total_text = 0
+    total_sentences = 0
+    total_syylables = 0
+    score = 0
+
+    print(text)
+
+    compute_readibility(ch1text.text)
