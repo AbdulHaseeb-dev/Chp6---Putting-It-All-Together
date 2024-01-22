04ec2aa36de00ac3f4b35ce4a354660d37652db2
AbdulHaseeb-dev <haseebrana.faisal@gmail.com>
2024-01-22 16:30:26 +0500

    total_text function

--- analyze.py
+++ analyze.py
@@ -5,6 +5,12 @@ def compute_readibility(text):
     total_syylables = 0
     score = 0
 
-    print(text)
+    words = text.split()
+    total_text = len(words)
+
+    print(words)
+    print(total_text, 'words')
+    
 
     compute_readibility(ch1text.text)
+    
---
+++ practice.py
@@ -0,0 +1,3 @@
+lyrics = 'I heard you on the wireless back in fifty two'
+for char in lyrics:
+    print(char)
