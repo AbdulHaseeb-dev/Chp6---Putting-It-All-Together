d89de51f96e85b8272563449cf0a061d9c27a63d
AbdulHaseeb-dev <haseebrana.faisal@gmail.com>
2024-01-22 16:46:37 +0500

    made count_sentences function

--- analyze.py
+++ analyze.py
@@ -1,4 +1,13 @@
 import ch1text
+
+def count_sentences(text):
+    count = 0
+
+    for char in text:
+        if char == '.' or char == ';' or char == '?' or char == '!':
+            count = count + 1
+    return count
+
 def compute_readibility(text):
     total_text = 0
     total_sentences = 0
@@ -7,10 +16,9 @@ def compute_readibility(text):
 
     words = text.split()
     total_text = len(words)
+    total_sentences = count_sentences(text)
+    print(total_text,'words')
+    print(total_sentences,'sentences')
 
-    print(words)
-    print(total_text, 'words')
-    
 
-    compute_readibility(ch1text.text)
-    
+compute_readibility(ch1text.text)
